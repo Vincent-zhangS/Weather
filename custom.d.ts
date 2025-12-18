18e3d9239e57db6c05b6f0d3f79ae7c82628c2c6
Vincent-zhangS <vzhang2022@gmail.com>
2025-12-18 00:14:10 -0500

    added work from 12-10 and finxed custom.d.ts

--- custom.d.ts
+++ custom.d.ts
@@ -1,14 +1,14 @@
-1 type responseItemType = {
-2   id: string;
-3   name: string;
-4 }
-5 
-6 type WeatherDetailType = {
-7   zipcode: string;
-8   weather: string;
-9   temp?: number;
-10 }
-11 
-12 interface WeatherQueryInterface {
-13   zipcode: string;
-14 }
+type responseItemType = {
+    id: string;
+    name: string;
+}
+
+type WeatherDetailType = {
+    zipcode: string;
+    weather: string;
+    temp?: number;
+}
+
+interface WeatherQueryInterface {
+    zipcode: string;
+}
--- index.ts
+++ index.ts
@@ -1,27 +1,37 @@
 import { routeHello, routeAPINames, routeWeather } from "./routes.js";
-import express, {Request,  Response} from "express";
+import express, { Request, Response } from "express";
+import path from "path"; // <-added this
 const server = express();
 const port = 3040;
-server.get("/hello", function (req: Request, res:Response):void {
-    const response = routeHello();
-    res.send(response);
+
+server.get("/components/weather", function (req: Request, res: Response): void {
+  const filePath = path.join(process.cwd(), "public", "weather.html");
+  res.setHeader("Content-Type", "text/html");
+  res.sendFile(filePath);
 });
-server.get("/api/names", async function (req: Request, res:Response):Promise<void> {
+server.get("/hello", function (req: Request, res: Response): void {
+  const response = routeHello();
+  res.send(response);
+});
+server.get(
+  "/api/names",
+  async function (req: Request, res: Response): Promise<void> {
     let response: string;
     try {
-        response = await routeAPINames();
+      response = await routeAPINames();
     } catch (err) {
-        console.log(err);
+      console.log(err);
     }
     res.send(response);
-});
+  }
+);
 
-server.get("/api/weather/:zipcode"), function (req: Request, res:Response):void {
+server.get("/api/weather/:zipcode"),
+  function (req: Request, res: Response): void {
     const response = routeWeather({ zipcode: req.params.zipcode });
     res.send(response);
-}
-
+  };
 
 server.listen(port, function () {
-    console.log("Listening on " + port);
+  console.log("Listening on " + port);
 });
